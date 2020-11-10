ee4f155063450a289f056b666a024cd037962e4c
Robin Desorme <rdesorme@webqam.fr>
2020-11-10 21:36:01 +0000

    install SASS Boilerplate

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,46 @@
-require('./bootstrap');
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+import './bootstrap';
+import Vue from 'vue';
+import { pascalToKebab } from './utils/String';
+
+import ExampleComponent from './components/ExampleComponent';
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+// Instantiate Vue root components
+[ExampleComponent].forEach((c) => {
+  if (process.env.NODE_ENV === 'development' && !c.name) {
+    throw new Error('Component name is undefined.');
+  }
+
+  Vue.component(c.name, c);
+
+  Array.from(document.getElementsByTagName(pascalToKebab(c.name))).forEach(
+    (el) => new Vue({ el })
+  );
+});
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// const app = new Vue({
+//   el: '#app',
+// });
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,62 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.extend('customConfig', webpackConfig => {
+  // Register "@" alias
+  webpackConfig.resolve.alias = {
+    vue$: 'vue/dist/vue.common.js', // Already exists
+    '@': __dirname + '/resources',
+  };
+
+  // Import the sass lib in every Sass file
+  // Doing it manually since options in .sass(src, dest, options) doesn't work with .vue files
+  webpackConfig.module.rules.forEach(r => {
+    ['loaders', 'use'].forEach(key => {
+      if (key in r) {
+        r[key].forEach(u => {
+          if (u.loader === 'sass-loader') {
+            if (!u.options) {
+              u.options = {};
+            }
+
+            u.options.prependData = '@import "@/scss/lib/_lib.scss";';
+          }
+        });
+      }
+    });
+  });
+});
+
+mix.webpackConfig({
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        test: /\.(js|vue)?$/,
+      },
+    ],
+  },
+});
+
+mix
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/scss/main.scss', 'public/css')
+  .options({ extractVueStyles: true })
+  .customConfig()
+  .version(['public/images']);
+
+if (mix.inProduction()) {
+  mix.styles(
+    ['node_modules/normalize.css/normalize.css', 'public/css/main.css'],
+    'public/css/main.css'
+  );
+} else {
+  mix
+    .styles(
+      'node_modules/normalize.css/normalize.css',
+      'public/css/normalize.css'
+    )
+    .sourceMaps();
+}
