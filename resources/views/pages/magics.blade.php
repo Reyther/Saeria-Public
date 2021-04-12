745d2f5f1f2bb1dd45ac4d7a16625b19d14843a3
Reyther <69626662+Reyther@users.noreply.github.com>
2021-04-13 00:33:56 +0200

    Magic-images Done

--- resources/views/pages/magics.blade.php
+++ resources/views/pages/magics.blade.php
@@ -5,27 +5,31 @@
 @section('content')
 
     <h1> Magies </h1>
-    <div class="row">
-    <div class="col-10 offset-1">
-    <ul class="row magic__container">
-    @foreach ($magics as $key => $circle )
-        <li class="col-md-6 magic__block">
-            <h2 class="magic__circle-name"> {{$key}} </h2>
-            <ul class="magic__circle">
-            @foreach ($circle as $magic)
-                <li class="magic">
-                    <img class="magic__image" src="{{$magic->image}}">
-                    <div class="magic__text">
-                        <h3 class="magic__title"> {{$magic->name}} </h3>
-                        <p class="magic__desc"> {{$magic->description}} </p>
-                    </div>
-                 </li>
-                @endforeach
-             </ul>
-         </li>
-    @endforeach
-    </ul>
-    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-10 offset-1">
+                <ul class="row magic__container">
+                    @foreach ($magics as $key => $circle )
+                        <li class="col-md-6 magic__block">
+                            <h2 class="magic__circle-name"> {{$key}} </h2>
+                            <ul class="magic__circle">
+                            @foreach ($circle as $magic)
+                                <li class="magic">
+                                    @if ($magic->image)
+                                    <img class="magic__image" width="150" height="150" src="/storage/{{$magic->image}}" alt="{{$magic->name}}">
+                                    @endif
+                                    <div class="magic__text">
+                                        <h3 class="magic__title"> {{$magic->name}} </h3>
+                                        <p class="magic__desc"> {{$magic->description}} </p>
+                                    </div>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 
 @endsection
